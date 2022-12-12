0218ff32853da97911d5bc6441d7cac595ccdcaa
SaraZM <sara.zapata-marin@mail.mcgill.ca>
2022-12-12 17:46:01 -0500

    analyze only Ozone data for Chapter 11, clean the data and change plots to ggplot

--- data_munge.R
+++ data_munge.R
@@ -15,3 +15,30 @@ uk_o3_uni <- data.frame(longitude = coords$longitude,
 write.csv(uk_o3_uni, "data/uk_ozone_uni.csv")
 
 
+# Chapter 11 -----
+
+## Daily data ----
+
+ozone_day <- read.csv("data/O3_2013_LA_sites.csv")
+
+site_day <- ozone_day[ozone_day$AQS_SITE_ID == "06-037-9033",] %>% 
+  select(Date, Daily.Max.8.hour.Ozone.Concentration)
+
+
+colnames(site_day) <- c("date", "max.ozone")
+
+#write.csv(site_day, "data/LA_daily.csv", row.names = FALSE)
+
+## Hourly data -----
+
+ozone_hour <- read.csv("data/O3_2013_Hourly_LA_Site840060370113.csv")
+
+site_hour <- select(ozone_hour, datetime, value)
+
+colnames(site_hour) <- c("datetime", "ozone")
+
+site_hour$time <- 1:nrow(site_hour)
+# site_hour <- tidyr::separate(site_hour, datetime, c("date", "time"), sep = "T")
+
+
+write.csv(site_hour, "data/LA_hourly_ozone.csv", row.names = FALSE)
